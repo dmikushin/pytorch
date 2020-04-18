298343962d6ab309134e05181ff0a82f211289b3
Dmitry Mikushin <dmitry@kernelgen.org>
2020-04-18 18:04:01 +0200

    Changing the code to unbreak Clang 11.0.3 internal error

--- aten/src/ATen/native/SobolEngineOps.cpp
+++ aten/src/ATen/native/SobolEngineOps.cpp
@@ -129,21 +129,19 @@ Tensor& _sobol_engine_initialize_state_(Tensor& sobolstate, int64_t dimension) {
 
   /// Use a tensor accessor for `sobolstate`
   auto ss_a = sobolstate.accessor<int64_t, 2>();
-  for (int64_t d = 0; d < dimension; ++d) {
+  for (int32_t d = 0; d < dimension; ++d) {
     int64_t p = poly[d];
-    int64_t m = bit_length(p) - 1;
+    int32_t m = bit_length(p) - 1;
 
-    for (int64_t i = 0; i < m; ++i) {
-      ss_a[d][i] = initsobolstate[d][i];
-    }
+    memcpy(&ss_a[d][0], &initsobolstate[d][0], m * sizeof(ss_a[d][0]));
 
-    for (int64_t j = m; j < MAXBIT; ++j) {
+    for (int32_t j = m; j < MAXBIT; ++j) {
       int64_t newv = ss_a[d][j - m];
-      int64_t pow2 = 1;
-      for (int64_t k = 0; k < m; ++k) {
+      int64_t pow2 = 1LL;
+      for (int32_t k = 0; k < m; ++k) {
         pow2 <<= 1;
-        if ((p >> (m - 1 - k)) & 1) {
-          newv = newv ^ (pow2 * ss_a[d][j - k - 1]);
+        if ((p >> (m - 1 - k)) & 1LL) {
+          newv ^= pow2 * ss_a[d][j - k - 1];
         }
       }
       ss_a[d][j] = newv;
